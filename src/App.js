026cc2acc152ddd85293e8261d833f97b7451d7c
Mo Arab <dema.a3rab@gmail.comgit config --global user.name MoAarab2>
2021-05-02 16:40:30 +0200

    added helmet

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Draw from '../src/pages/draw/draw';
 import Footer from '../src/components/footer/footer'
 import SubscribePage from '../src/pages/contact/contact';
 import About from '../src/pages/about/about';
+import Helmet from "react-helmet";
 
 //import Header from '../src/components/header/header.co';
 
@@ -16,6 +17,48 @@ import About from '../src/pages/about/about';
 function App() {
   return (
     <div className="App">
+    <Helmet>
+    <meta
+      name="description"
+      content="Mo Arab artist page, for viewing my artworks"
+    />
+
+    {/* <!-- Google / Search Engine Tags --> */}
+    <meta itemprop="name" content="Mo Arab Art" />
+    <meta
+      itemprop="description"
+      content="Mo Arab artist page, for viewing my artworks"
+    />
+    <meta
+      itemprop="image"
+      content="https://www.moarab.com/static/media/image1tum.63d31e08.jpg"
+    />
+
+    {/* <!-- Facebook Meta Tags --> */}
+    <meta property="og:url" content="https://www.moarab.com" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="Mo Arab Art" />
+    <meta
+      property="og:description"
+      content="Mo Arab artist page, for viewing my artworks"
+    />
+    <meta
+      property="og:image"
+      content="https://www.moarab.com/static/media/image1tum.63d31e08.jpg"
+    />
+
+    {/* <!-- Twitter Meta Tags --> */}
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:title" content="Mo Arab Art" />
+    <meta
+      name="twitter:description"
+      content="Mo Arab artist page, for viewing my artworks"
+    />
+    <meta
+      name="twitter:image"
+      content="https://www.moarab.com/static/media/image1tum.63d31e08.jpg"
+    />
+    </Helmet>
       <Navbar />
      
       <div>
